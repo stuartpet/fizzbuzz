63515adafb89e32660a6cd17e02a1496ba5db332
Stuart <stuart.pethurst@gmail.com>
2019-03-25 13:29:00 +0000

    fizzbuzz tests and code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(number)
-  'fizz'
+  if number % 3 == 0 and number % 5 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  else number % 5 == 0
+    return 'buzz'
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+end
